95f59fbb9a69536cc13a7f8cd79a5753fd86d81d
SuelenWagner <siwc94@gmail.com>
2022-01-14 22:44:18 -0300

    Add save score

--- backend/src/main/java/com/devsuperior/dsmovie/entities/ScorePK.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/ScorePK.java
@@ -1,7 +1,6 @@
 package com.devsuperior.dsmovie.entities;
 
 import java.io.Serializable;
-
 import javax.persistence.Embeddable;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
